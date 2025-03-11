5ef09d7b0d615a245ee997364c824f3cd8b04380
Rejoice2374 <adeboyerejoice2374@gmail.com>
2025-03-11 15:40:02 +0100

    navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-
 import Logo from "../src/assets/logo.png";
+import { useState } from "react";
+import { navLinks } from "../constants";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <header className="bg-white">
       <nav
@@ -25,7 +28,39 @@ const Navbar = () => {
           <a
             href="#"
             className="text-base font-medium text-gray-900 hover:text-gray-900"
-          ></a>
+          >
+            Contact
+          </a>
+        </div>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          {toggleMenu ? (
+            <XMarkIcon
+              className="hamburger text-black cursor-pointer w-6"
+              onClick={() => setToggleMenu(false)}
+              aria-label="Close menu"
+            />
+          ) : (
+            <Bars3Icon
+              className="hamburger text-black cursor-pointer w-6"
+              onClick={() => setToggleMenu(true)}
+              aria-label="Open menu"
+            />
+          )}
+          {toggleMenu && (
+            <div className="flex flex-col p-6 bg-black absolute top-10 mx-4 my-2 rounded-xl sidebar self-end font-bold sm:self-center left-6 right-6 drop-shadow-md">
+              <ul className="list-none flex flex-col justify-end items-center space-x-0 space-y-4 flex-1">
+                {navLinks.map((nav) => (
+                  <li
+                    key={nav.id}
+                    className="font-poppins font-normal cursor-pointer text-[16px] text-white"
+                  >
+                    <a href={`#${nav.id}`}>{nav.title}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       </nav>
     </header>
---
+++ constants/index.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "services",
+    title: "Services",
+  },
+  {
+    id: "project",
+    title: "Project",
+  },
+  {
+    id: "contacts",
+    title: "Contacts",
+  },
+];
