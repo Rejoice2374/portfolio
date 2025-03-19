6e749c1659d89904e86fc2c480c13b674ccf634a
Rejoice2374 <adeboyerejoice2374@gmail.com>
2025-03-19 13:11:40 +0100

    hero

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -13,27 +13,23 @@ const Navbar = () => {
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <a href="#" className="flex gap-2 items-center">
             <img alt="" src={Logo} className="h-8 w-auto inline" />
-            <span className="text-xl text-black">Favochino Tech</span>
+            <h1 className="text-xl text-blue-600">Favochino Tech</h1>
           </a>
         </div>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-center lg:space-x-12">
-          <a
-            href="#"
-            className="text-base font-medium text-gray-900 hover:text-gray-900"
-          >
-            Home
-          </a>
-          <a
-            href="#"
-            className="text-base font-medium text-gray-900 hover:text-gray-900"
-          >
-            Contact
-          </a>
+        <div className="hidden lg:flex gap-12">
+          {navLinks.map((nav) => (
+            <a
+              href="#"
+              className="text-xl font-medium text-gray-900 hover:underline decoration-blue-400"
+            >
+              {nav.title}
+            </a>
+          ))}
         </div>
 
-        <div className="sm:hidden flex flex-1 justify-end items-center">
+        <div className="lg:hidden flex flex-1 justify-end items-center">
           {toggleMenu ? (
             <XMarkIcon
               className="hamburger text-black cursor-pointer w-6"
@@ -48,17 +44,17 @@ const Navbar = () => {
             />
           )}
           {toggleMenu && (
-            <div className="flex flex-col p-6 bg-black absolute top-10 mx-4 my-2 rounded-xl sidebar self-end font-bold sm:self-center left-6 right-6 drop-shadow-md">
-              <ul className="list-none flex flex-col justify-end items-center space-x-0 space-y-4 flex-1">
-                {navLinks.map((nav) => (
+            <div className="flex flex-col p-6 bg-white absolute top-10 mx-4 my-10 z-1 sidebar self-end font-bold sm:self-center left-6 right-6 drop-shadow-md">
+              {navLinks.map((nav) => (
+                <ul className="list-none flex flex-col justify-end items-center space-x-0 space-y-4 flex-1">
                   <li
                     key={nav.id}
-                    className="font-poppins font-normal cursor-pointer text-[16px] text-white"
+                    className="font-poppins font-normal cursor-pointer text-[16px] text-black"
                   >
                     <a href={`#${nav.id}`}>{nav.title}</a>
                   </li>
-                ))}
-              </ul>
+                </ul>
+              ))}
             </div>
           )}
         </div>
