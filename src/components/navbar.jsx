df84f7e33fcabbf511e69419cb3b0b92dbb8501a
Rejoice2374 <adeboyerejoice2374@gmail.com>
2025-05-25 00:06:36 +0100

    activeLink done

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,50 @@
 import React from "react";
 import Logo from "../assets/Ft-logoText-purple.png";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { navLinks, navIcons } from "../constants";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
+  const location = useLocation();
+  const [activeLink, setActiveLink] = useState("");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Extract the hash from the URL (e.g., "#home" from "http://example.com/#home")
+    const hash = location.hash;
+    if (hash) {
+      setActiveLink(hash.substring(1)); // Remove the '#' to match with path in navLinks
+    } else {
+      // Set default active link if no hash is present
+      setActiveLink(navLinks[0]?.path.toLowerCase() || "");
+    }
+  }, [location]);
+
   return (
-    <header className="bg-dark">
+    <header
+      className={`${
+        scrolled ? "bg-secondary p-0" : "bg-transparent"
+      } fixed top-0 w-full p-3 lg:px-4 z-50 transition-all duration-300 ease-in-out`}
+    >
       <nav
         aria-label="Global"
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 "
+        className="mx-auto flex max-w-7xl items-center justify-between"
       >
         <div className="flex lg:flex-1">
           <a href="#" className="flex gap-2 items-center">
@@ -22,9 +55,18 @@ const Navbar = () => {
           {navLinks.map((n) => (
             <li
               key={n.id}
-              className="p-[16px]  text-button hover:text-coolAsh transition duration-300 ease-in-out"
+              className={`p-1 text-button hover:text-coolAsh transition duration-300 ease-in-out ${
+                activeLink === n.path.toLowerCase()
+                  ? "border-b-2 border-button"
+                  : ""
+              }`}
             >
-              <Link to={`#${n.path.toLowerCase()}`}>{n.title}</Link>
+              <Link
+                to={`#${n.path.toLowerCase()}`}
+                onClick={() => setActiveLink(n.path.toLowerCase())}
+              >
+                {n.title}
+              </Link>
             </li>
           ))}
         </ul>
@@ -62,9 +104,21 @@ const Navbar = () => {
                 {navLinks.map((n) => (
                   <li
                     key={n.id}
-                    className="p-[16px] text-button hover:text-coolAsh transition duration-300 ease-in-out"
+                    className={`p-[16px] text-button hover:text-coolAsh transition duration-300 ease-in-out ${
+                      activeLink === n.path.toLowerCase()
+                        ? "border-b-2 border-blue-500"
+                        : ""
+                    }`}
                   >
-                    <Link to={`#${n.path.toLowerCase()}`}>{n.title}</Link>
+                    <Link
+                      to={`#${n.path.toLowerCase()}`}
+                      onClick={() => {
+                        setActiveLink(n.path.toLowerCase());
+                        setToggleMenu(false);
+                      }}
+                    >
+                      {n.title}
+                    </Link>
                   </li>
                 ))}
               </ul>
