7a2fa23f4115b7898358c8c571c1c44d617d1f8c
Rejoice2374 <adeboyerejoice2374@gmail.com>
2025-08-16 00:58:37 +0100

    contact section

--- src/components/Home/cta.jsx
+++ src/components/Home/cta.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { useState } from "react";
 
 const Cta = () => {
-  return <div>Cta</div>;
+  const formInitialDetails = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
+
+  const [formDetails, setFormDetails] = useState(formInitialDetails);
+  const [buttonText, setButtonText] = useState("Send");
+  const [status, setStatus] = useState({});
+
+  return (
+    <>
+      <section className="contact" id="connect">
+        <div className="flex items-center">
+          <div className="image">
+            <img src={} alt="Contact Us" />
+          </div>
+          <div className="right">
+            <h2>Get In Touch</h2>
+            <form>
+               
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  )
 };
 
 export default Cta;
